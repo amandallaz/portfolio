6d3e4033f4b7b1dc6217d88716727f864a2ff0ab
amandallaz <allazdev@gmail.com>
2025-06-15 09:50:38 -0400

    Clean up for prod,  add compiled Tailwind CSS
    Create separate requirements, fix small path errors, add compiled Tailwind CSS

--- main_site/main_site/settings.py
+++ main_site/main_site/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 ### Added
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'theme' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
