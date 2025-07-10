c3e465945220f3062759e93f4d65a0bd0f998867
amandallaz <allazdev@gmail.com>
2025-07-09 22:52:51 -0400

    small categories and description updates

--- main_site/portfolio/models.py
+++ main_site/portfolio/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 CATEGORY_CHOICES = [
     ('apps', 'Apps'),
-    ('data science', 'Data Science'),
-    ('visualization', 'Visualization'),
+    ('projects', 'Projects')
+
 ]
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='project_images/')
     url = models.URLField(blank=True)
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='visualization')  # <-- NEW
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='apps')  
 
     def __str__(self):
         return self.title
