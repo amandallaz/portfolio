729f0490d9f27248c40708c30c76ab10a2279844
amandallaz <allazdev@gmail.com>
2026-02-03 15:03:32 -0500

    add photo gallery infra

--- main_site/portfolio/models.py
+++ main_site/portfolio/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 CATEGORY_CHOICES = [
     ('apps', 'Apps'),
-    ('projects', 'Projects')
+    ('projects', 'Projects'),
+    ('photos', 'Photos'),
 
 ]
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='project_images/')
     url = models.URLField(blank=True)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='apps')  
